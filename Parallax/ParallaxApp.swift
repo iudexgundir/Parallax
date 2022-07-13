f4d91922753a6590a513a18c27fda37b20219070
iudexgundir <govoroverx@icloud.com>
2022-07-13 22:50:53 +0300

    Initial Commit

---
+++ Parallax/ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by Эрхаан Говоров on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
